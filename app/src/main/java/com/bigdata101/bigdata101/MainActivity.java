f140d0c17621e122f87a97e32de26de918dd45be
mustard123 <silasdavidweber@gmail.com>
2018-05-01 13:14:02 +0200

    expendable listview in navigation drawer feature

--- app/src/main/java/com/bigdata101/bigdata101/MainActivity.java
+++ app/src/main/java/com/bigdata101/bigdata101/MainActivity.java
@@ -18,15 +18,24 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
+import android.widget.ExpandableListView;
 import android.widget.ListView;
 import android.widget.PopupWindow;
+import android.widget.Toast;
 
 import com.bigdata101.bigdata101.constants.Constants;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
 
 public class MainActivity extends AppCompatActivity implements MyFragmentInteraction {
+
+
+    ExpandableListAdapter listAdapter;
+    ExpandableListView expListView;
+    List<String> listDataHeader;
+    HashMap<String, List<String>> listDataChild;
 
     private DrawerLayout drawerLayout;
     private ListView drawerList;
@@ -64,14 +73,37 @@ public class MainActivity extends AppCompatActivity implements MyFragmentInterac
         Log.d("legal", Boolean.toString(accepted_legal_disclaimer));
         accepted_legal_disclaimer = sharedPreferences.getBoolean("legal_accepted", false);
         drawerLayout = findViewById(R.id.drawer_layout);
-        drawerList = findViewById(R.id.left_drawer);
 
-        final List<String> drawerMenuesList = new ArrayList<String>();
-        drawerMenuesList.add("Home");
-        drawerMenuesList.add("Introduction");
-        drawerMenuesList.add("Technology news");
-        drawerMenuesList.add("Law news");
-        drawerMenuesList.add("Chapters");
+        // get the listview
+        expListView = (ExpandableListView) findViewById(R.id.left_drawer);
+
+        // preparing list data
+        prepareListData();
+
+        listAdapter = new ExpandableListAdapter(this, listDataHeader, listDataChild);
+
+        // setting list adapter
+        expListView.setAdapter(listAdapter);
+
+        // Listview on child click listener
+        expListView.setOnChildClickListener(new ExpandableListView.OnChildClickListener() {
+
+            @Override
+            public boolean onChildClick(ExpandableListView parent, View v,
+                                        int groupPosition, int childPosition, long id) {
+                // TODO Auto-generated method stub
+                Toast.makeText(
+                        getApplicationContext(),
+                        listDataHeader.get(groupPosition)
+                                + " : "
+                                + listDataChild.get(
+                                listDataHeader.get(groupPosition)).get(
+                                childPosition), Toast.LENGTH_SHORT)
+                        .show();
+                return false;
+            }
+        });
+
 
 
 
@@ -79,90 +111,11 @@ public class MainActivity extends AppCompatActivity implements MyFragmentInterac
                 .replace(R.id.fragment_container, WelcomeFragment.newInstance(null, null))
                 .commit();
 
-        stringAdaptor = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, drawerMenuesList);
-        drawerList.setAdapter(stringAdaptor);
 
 
 
-        drawerList.setOnItemClickListener(new AdapterView.OnItemClickListener() {
-            @Override
-            public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
-                String selectedFromList = adapterView.getItemAtPosition(i).toString();
-
-                if (selectedFromList.equals("Chapters") && !clicked){
-                    Log.d("chapterclick", "subchapter");
-                    drawerMenuesList.add("Subchapter1");
-                    drawerMenuesList.add("Subchapter2");
-                    drawerMenuesList.add("Subchapter3");
-                    clicked = true;
-                    stringAdaptor.notifyDataSetChanged();
-                    return;
-                }
-                if (selectedFromList.equals("Chapters") && clicked){
-                    drawerMenuesList.remove("Subchapter1");
-                    drawerMenuesList.remove("Subchapter2");
-                    drawerMenuesList.remove("Subchapter3");
-                    clicked = false;
-                    stringAdaptor.notifyDataSetChanged();
-                    return;
-                }
-                if (selectedFromList.equals("Home")) {
-                    Log.d("clicked", "home");
-                    for (Fragment fragment : getSupportFragmentManager().getFragments()) {
-
-                        if (fragment != null)
-                            getSupportFragmentManager().beginTransaction().remove(fragment).commit();
-
-                    }
-
-                    getSupportFragmentManager().beginTransaction()
-                            .replace(R.id.fragment_container, WelcomeFragment.newInstance(null, null))
-                            .commit();
 
 
-                    drawerLayout.closeDrawer(GravityCompat.START);
-                }
-                if (selectedFromList.equals("Introduction")){
-                    Log.d("clicked", "introduction");
-                    getSupportFragmentManager()
-                            .beginTransaction()
-                            .replace(R.id.fragment_container, BigData101Fragment.newInstance(null,null))
-                            .addToBackStack(null)
-                            .commit();
-
-                    drawerLayout.closeDrawer(GravityCompat.START);
-                }
-                if (selectedFromList.equals("Technology news")){
-                    Log.d("clicked", "tech news");
-                    getSupportFragmentManager()
-                            .beginTransaction()
-                            .replace(R.id.fragment_container, RecyclerViewFragment.newInstance(techArticlesEndpoint,null), newsFragmentTag)
-                            .addToBackStack("techFragment").
-                            commit();
-                    drawerLayout.closeDrawer(GravityCompat.START);
-                }
-                if (selectedFromList.equals("Law news")){
-                    Log.d("clicked", "tech news");
-                    getSupportFragmentManager()
-                            .beginTransaction()
-                            .replace(R.id.fragment_container, RecyclerViewFragment.newInstance(lawArticlesEndpoint,null), newsFragmentTag)
-                            .addToBackStack("lawFragment")
-                            .commit();
-                    drawerLayout.closeDrawer(GravityCompat.START);
-                }
-                if (selectedFromList.equals("Subchapter1")){
-                    Log.d("clicked", "subchapter1");
-                    getSupportFragmentManager()
-                            .beginTransaction()
-                            .replace(R.id.fragment_container, Subchapter1Fragment.newInstance(null,null))
-                            .addToBackStack(null)
-                            .commit();
-
-                    drawerLayout.closeDrawer(GravityCompat.START);
-                }
-
-            }
-        });
 
 
 
@@ -319,4 +272,44 @@ public class MainActivity extends AppCompatActivity implements MyFragmentInterac
         }
 
     }
+
+    private void prepareListData() {
+        listDataHeader = new ArrayList<String>();
+        listDataChild = new HashMap<String, List<String>>();
+
+        // Adding child data
+        listDataHeader.add("Top 250");
+        listDataHeader.add("Now Showing");
+        listDataHeader.add("Coming Soon..");
+
+        // Adding child data
+        List<String> top250 = new ArrayList<String>();
+        top250.add("The Shawshank Redemption");
+        top250.add("The Godfather");
+        top250.add("The Godfather: Part II");
+        top250.add("Pulp Fiction");
+        top250.add("The Good, the Bad and the Ugly");
+        top250.add("The Dark Knight");
+        top250.add("12 Angry Men");
+
+        List<String> nowShowing = new ArrayList<String>();
+        nowShowing.add("The Conjuring");
+        nowShowing.add("Despicable Me 2");
+        nowShowing.add("Turbo");
+        nowShowing.add("Grown Ups 2");
+        nowShowing.add("Red 2");
+        nowShowing.add("The Wolverine");
+
+        List<String> comingSoon = new ArrayList<String>();
+        comingSoon.add("2 Guns");
+        comingSoon.add("The Smurfs 2");
+        comingSoon.add("The Spectacular Now");
+        comingSoon.add("The Canyons");
+        comingSoon.add("Europa Report");
+
+        listDataChild.put(listDataHeader.get(0), top250); // Header, Child data
+        listDataChild.put(listDataHeader.get(1), nowShowing);
+        listDataChild.put(listDataHeader.get(2), comingSoon);
+    }
 }
+
