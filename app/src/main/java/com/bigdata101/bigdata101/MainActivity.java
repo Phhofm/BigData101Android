797f50c021458ff6ca70cfff3bde37e1cac39f2e
mustard123 <silasdavidweber@gmail.com>
2018-05-06 19:41:15 +0200

    minor final corrections

--- app/src/main/java/com/bigdata101/bigdata101/MainActivity.java
+++ app/src/main/java/com/bigdata101/bigdata101/MainActivity.java
@@ -324,7 +324,7 @@ public class MainActivity extends AppCompatActivity implements MyFragmentInterac
         List<String> scopeSubchapters = Arrays.asList("Theory","Examples");
 
 
-        List<String> keypointsSubchapters = Arrays.asList("GDPR principles","Accountability Art.5 § 2", "Records of processing activities Art.30",
+        List<String> keypointsSubchapters = Arrays.asList("GDPR principles Art.5 § 1","Accountability Art.5 § 2", "Records of processing activities Art.30",
                 "Right to erasure Art.17","Right to data portability Art.20","Privacy by design and default Art.25",
                 "Processor and controller Art.24 / 26-29","Security Art.32", "Reporting Art.33",
                 "Data protection impact assessment Art.35","Data protection officer Art.37");
